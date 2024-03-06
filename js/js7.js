0c65e3e659422872c1f2917afe3e9e10d400e315
Subhrajitt <Subhrajit@influxiqtech.com>
2024-03-06 19:29:28 +0530

    pull git commit -m 'pull

---
+++ js/js7.js
@@ -0,0 +1,1 @@
+const isSafari = /constructor/i.test(window.HTMLElement) || "[object SafariRemoteNotification]" === (!window.safari || "undefined" != typeof safari && safari.pushNotification).toString(); isSafari && document.body.classList.add("safari");
